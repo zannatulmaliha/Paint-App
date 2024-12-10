edc1d480271318cb15258b1db94ff2862f68eef2
zannatulmaliha <147336779+zannatulmaliha@users.noreply.github.com>
2024-12-10 18:59:46 +0600

    update animation.h

--- animation.h
+++ animation.h
@@ -1,19 +1,19 @@
 #include <wx/wx.h>
 #include <wx/timer.h>
+#include "custombutton2.h"
 
-// A custom panel for drawing shapes (canvas)
 class AnimationCanvas : public wxPanel {
 public:
     AnimationCanvas(wxWindow* parent, wxWindow* mainMenu)
-        : wxPanel(parent), mainMenu(mainMenu) { // Initialize mainMenu properly here
+        : wxPanel(parent), mainMenu(mainMenu) {
 
-        // Create buttons on the canvas
-        shapesButton = new wxButton(this, wxID_ANY, "Shapes", wxPoint(20, 300));
-        selectButton = new wxButton(this, wxID_ANY, "Select", wxPoint(120, 300));
-        animationButton = new wxButton(this, wxID_ANY, "Animation", wxPoint(220, 300));
-        returntoMain = new wxButton(this, wxID_ANY, "Return to Main Menu", wxPoint(320, 300));
+         SetBackgroundColour(wxColour(255, 240,245));
 
-        // Bind button events to show submenus
+         shapesButton = new CustomButton2(this, wxID_ANY, "Shapes", wxPoint(20, 30), wxSize(100, 50));
+        selectButton = new CustomButton2(this, wxID_ANY, "Select", wxPoint(140, 30), wxSize(100, 50));
+        animationButton = new CustomButton2(this, wxID_ANY, "Animation", wxPoint(260, 30), wxSize(100, 50));
+        returntoMain = new CustomButton2(this, wxID_ANY, "Return to Main Menu", wxPoint(380, 30), wxSize(150, 50));
+
         shapesButton->Bind(wxEVT_BUTTON, &AnimationCanvas::OnShapesClicked, this);
         selectButton->Bind(wxEVT_BUTTON, &AnimationCanvas::OnSelectClicked, this);
         animationButton->Bind(wxEVT_BUTTON, &AnimationCanvas::OnAnimationClicked, this);
@@ -21,43 +21,45 @@ public:
     }
 
 private:
-    wxButton* shapesButton;
-    wxButton* selectButton;
-    wxButton* animationButton;
-    wxButton* returntoMain;
-    wxWindow* mainMenu;  // To store the main menu pointer
+     CustomButton2* shapesButton;
+    CustomButton2* selectButton;
+    CustomButton2* animationButton;
+    CustomButton2* returntoMain;
+    wxWindow* mainMenu;
 
-    // Event handlers for button clicks
+
     void OnShapesClicked(wxCommandEvent& event) {
-        // Implement functionality for the Shapes button here
+
     }
 
     void OnSelectClicked(wxCommandEvent& event) {
-        // Implement functionality for the Select button here
+
     }
 
     void OnAnimationClicked(wxCommandEvent& event) {
-        // Implement functionality for the Animation button here
+
     }
 
     void OnReturntoMainMenuClicked(wxCommandEvent& event) {
-        this->Close();  // Hide the animation frame
-        mainMenu->Show();  // Show the main menu
+        this->GetParent()->Close();
+        mainMenu->Show();
     }
 };
 
-// The main frame class that hosts the animation canvas
+
 class AnimationFrame : public wxFrame {
 public:
     AnimationFrame(wxWindow* parent)
         : wxFrame(parent, wxID_ANY, "Animation", wxDefaultPosition, wxSize(600, 400)) {
 
-        mainMenu = parent; // Store the reference to the main menu (parent)
+        mainMenu = parent;
 
-        // Create the custom animation canvas and pass the mainMenu pointer to it
+
         new AnimationCanvas(this, mainMenu);
+
     }
 
 private:
-    wxWindow* mainMenu;  // Store the main menu pointer
+    wxWindow* mainMenu;
+
 };
