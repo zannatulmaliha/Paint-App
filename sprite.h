a39ca55b8fc604322d0cc42b95607b0319cd744f
zannatulmaliha <147336779+zannatulmaliha@users.noreply.github.com>
2024-12-12 18:37:53 +0600

    Add files via upload

---
+++ sprite.h
@@ -0,0 +1,98 @@
+#include <wx/wx.h>
+#include <wx/dcbuffer.h>
+
+class Sprite {
+public:
+    Sprite(const wxString& filePath, int x, int y)
+        : x(x), y(y), width(100), height(100), isDragging(false), isResizing(false), isSelected(false) {
+
+        wxImage image;
+        if (image.LoadFile(filePath, wxBITMAP_TYPE_JPEG)) {
+            originalBitmap = wxBitmap(image);
+            spriteBitmap = wxBitmap(image.Scale(width, height, wxIMAGE_QUALITY_HIGH));
+        } else {
+            wxLogError("Failed to load image: %s", filePath);
+        }
+    }
+
+
+    void Draw(wxDC& dc) {
+        if (spriteBitmap.IsOk()) {
+            dc.DrawBitmap(spriteBitmap, x, y, true);
+
+            if (isSelected) {
+
+                dc.SetPen(*wxRED_PEN);
+                dc.SetBrush(*wxTRANSPARENT_BRUSH);
+                dc.DrawRectangle(x, y, width, height);
+            }
+        }
+    }
+
+
+    bool Contains(int mouseX, int mouseY) {
+        return (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height);
+    }
+
+    void SetSelected(bool selected) {
+        isSelected = selected;
+    }
+
+    bool IsSelected() const {
+        return isSelected;
+    }
+
+    void StartDragging(int mouseX, int mouseY) {
+        isDragging = true;
+        dragOffsetX = mouseX - x;
+        dragOffsetY = mouseY - y;
+    }
+
+    void StopDragging() {
+        isDragging = false;
+    }
+
+    void MoveTo(int mouseX, int mouseY) {
+        if (isDragging) {
+            x = mouseX - dragOffsetX;
+            y = mouseY - dragOffsetY;
+        }
+    }
+
+    void StartResizing(int mouseX, int mouseY) {
+
+        if (mouseX >= x + width - 10 && mouseY >= y + height - 10) {
+            isResizing = true;
+        }
+    }
+
+    void StopResizing() {
+        isResizing = false;
+    }
+
+    void Resize(int mouseX, int mouseY) {
+        if (isResizing) {
+            width = mouseX - x;
+            height = mouseY - y;
+
+
+            spriteBitmap = wxBitmap(originalBitmap.ConvertToImage().Scale(width, height, wxIMAGE_QUALITY_HIGH));
+        }
+    }
+
+    bool IsResizing() const {
+        return isResizing;
+    }
+
+private:
+    wxBitmap originalBitmap;
+    wxBitmap spriteBitmap;
+    int x, y;
+    int width, height;
+    bool isDragging;
+    bool isResizing;
+    bool isSelected;
+
+    int dragOffsetX;
+    int dragOffsetY;
+};
