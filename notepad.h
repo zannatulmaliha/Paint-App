ea4d253755cb700094ece62c3188a2aeb6f892cd
zannatulmaliha <147336779+zannatulmaliha@users.noreply.github.com>
2025-03-10 00:44:27 +0600

    Update notepad.h

--- notepad.h
+++ notepad.h
@@ -1,143 +1,62 @@
 #include <wx/wx.h>
 #include <wx/richtext/richtextctrl.h>
-#include <wx/colordlg.h>
-#include <wx/fontdlg.h>
+#include "custombutton3.h"  // Include the header file for CustomButton3
 
 class NotepadFrame : public wxFrame {
 public:
     NotepadFrame(wxWindow* parent)
         : wxFrame(parent, wxID_ANY, "Notepad", wxDefaultPosition, wxSize(600, 400)), mainMenu(parent) {
 
+        // Load the background image
+        backgroundBitmap = wxBitmap("notes.jpg", wxBITMAP_TYPE_JPEG); // Change to the path of your image
 
-        textCtrl = new wxRichTextCtrl(this, wxID_ANY, "", wxDefaultPosition, wxSize(600, 350), wxTE_MULTILINE);
+        // Bind the paint event to draw the background
+        Bind(wxEVT_PAINT, &NotepadFrame::OnPaint, this);
 
+        // Create custom buttons
+        typeButton = new CustomButton3(this, wxID_ANY, "Type", wxPoint(20, 30), wxSize(100, 35));
+        colorButton = new CustomButton3(this, wxID_ANY, "Color", wxPoint(140, 30), wxSize(100, 35));
+        fontButton = new CustomButton3(this, wxID_ANY, "fontsize", wxPoint(260, 30), wxSize(100, 35));
+        returntoMain = new CustomButton3(this, wxID_ANY, "Return to Main Menu", wxPoint(380, 30), wxSize(150, 35));
 
+     textCtrl = new wxRichTextCtrl(this, wxID_ANY, "", wxPoint(20, 80), wxSize(550, 280), wxRE_MULTILINE);
+        wxColour notepadBackgroundColor(255, 253, 208); // Light yellow, like a notepad
+        textCtrl->SetBackgroundColour(notepadBackgroundColor);
         textCtrl->SetFocus();
-
-
-        wxMenuBar* menuBar = new wxMenuBar();
-
-        wxMenu* fileMenu = new wxMenu();
-        fileMenu->Append(wxID_NEW, "&New\tCtrl-N", "Create a new document");
-        fileMenu->Append(wxID_OPEN, "&Open\tCtrl-O", "Open an existing document");
-        fileMenu->AppendSeparator();
-        fileMenu->Append(wxID_SAVE, "&Save\tCtrl-S", "Save the current document");
-        fileMenu->Append(wxID_SAVEAS, "Save &As\tCtrl-Shift-S", "Save the document with a new name");
-        fileMenu->AppendSeparator();
-        fileMenu->Append(wxID_EXIT, "E&xit\tAlt-X", "Quit the notepad");
-
-        wxMenu* textColorMenu = new wxMenu();
-        textColorMenu->Append(ID_TEXT_COLOR, "Text Color", "Change the text color");
-
-        wxMenu* fontSizeMenu = new wxMenu();
-        fontSizeMenu->Append(ID_FONT_SIZE, "Font Size", "Change the text size");
-
-        wxMenu* fontStyleMenu = new wxMenu();
-        fontStyleMenu->Append(ID_FONT_STYLE, "Font Style", "Change the text font style");
-
-        menuBar->Append(fileMenu, "&File");
-        menuBar->Append(textColorMenu, "&TextColor");
-        menuBar->Append(fontSizeMenu, "&FontSize");
-        menuBar->Append(fontStyleMenu, "&FontStyle");
-
-
-        SetMenuBar(menuBar);
-
-        Bind(wxEVT_MENU, &NotepadFrame::OnNew, this, wxID_NEW);
-        Bind(wxEVT_MENU, &NotepadFrame::OnOpen, this, wxID_OPEN);
-        Bind(wxEVT_MENU, &NotepadFrame::OnSave, this, wxID_SAVE);
-        Bind(wxEVT_MENU, &NotepadFrame::OnSaveAs, this, wxID_SAVEAS);
-        Bind(wxEVT_MENU, &NotepadFrame::OnExit, this, wxID_EXIT);
-
-        Bind(wxEVT_MENU, &NotepadFrame::OnTextColor, this, ID_TEXT_COLOR);
-        Bind(wxEVT_MENU, &NotepadFrame::OnFontSize, this, ID_FONT_SIZE);
-        Bind(wxEVT_MENU, &NotepadFrame::OnFontStyle, this, ID_FONT_STYLE);
     }
 
 private:
-    wxRichTextCtrl* textCtrl;
     wxWindow* mainMenu;
+    wxBitmap backgroundBitmap;
 
-    static const int ID_TEXT_COLOR = wxID_HIGHEST + 1;
-    static const int ID_FONT_SIZE = wxID_HIGHEST + 2;
-    static const int ID_FONT_STYLE = wxID_HIGHEST + 3;
+    CustomButton3* typeButton;
+    CustomButton3* colorButton;
+    CustomButton3* fontButton;
+    CustomButton3* returntoMain;
 
+     wxRichTextCtrl* textCtrl;
 
-    void OnNew(wxCommandEvent& event) {
-        textCtrl->Clear();
+   void OnPaint(wxPaintEvent& event) {
+    wxPaintDC dc(this);
+    wxSize size = GetClientSize();
+
+    // Check if the background bitmap is valid
+    if (backgroundBitmap.IsOk()) {
+        // Convert the bitmap to an image for scaling
+        wxImage backgroundImage = backgroundBitmap.ConvertToImage();
+
+        // Scale the image to fit the size of the frame
+        wxImage scaledImage = backgroundImage.Scale(size.GetWidth(), size.GetHeight());
+
+        // Convert the scaled image back to a bitmap
+        wxBitmap scaledBitmap(scaledImage);
+
+        // Draw the scaled bitmap as the background
+        dc.DrawBitmap(scaledBitmap, 0, 0, true); // The third argument is 'useMask', set it to true if you have transparency
     }
 
-    void OnOpen(wxCommandEvent& event) {
-        wxFileDialog openFileDialog(this, "Open Text File", "", "",
-                                    "Text files (*.txt)|*.txt", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
-        if (openFileDialog.ShowModal() == wxID_OK) {
-            textCtrl->LoadFile(openFileDialog.GetPath());
-        }
-    }
-
-    void OnSave(wxCommandEvent& event) {
-        wxFileDialog saveFileDialog(this, "Save Text File", "", "",
-                                    "Text files (*.txt)|*.txt", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
-        if (saveFileDialog.ShowModal() == wxID_OK) {
-            textCtrl->SaveFile(saveFileDialog.GetPath());
-        }
-    }
-
-    void OnSaveAs(wxCommandEvent& event) {
-        wxFileDialog saveFileDialog(this, "Save As Text File", "", "",
-                                    "Text files (*.txt)|*.txt", wxFD_SAVE | wxFD_OVERWRITE_PROMPT);
-        if (saveFileDialog.ShowModal() == wxID_OK) {
-            textCtrl->SaveFile(saveFileDialog.GetPath());
-        }
-    }
-
-    void OnExit(wxCommandEvent& event) {
-        this->Close();
-        mainMenu->Show();
-    }
-
-
-
-
-void OnTextColor(wxCommandEvent& event) {
-
-    wxColourDialog colorDlg(this);
-    if (colorDlg.ShowModal() == wxID_OK) {
-        wxColour color = colorDlg.GetColourData().GetColour();
-
-
-        wxRichTextAttr attr;
-        attr.SetTextColour(color);
-
-
-    }
+    event.Skip();  // Continue processing the event
 }
 
-
-
-
-
-    void OnFontSize(wxCommandEvent& event) {
-
-        wxTextEntryDialog dialog(this, "Enter font size:", "Font Size", "12");
-        if (dialog.ShowModal() == wxID_OK) {
-
-            long fontSize;
-            if (dialog.GetValue().ToLong(&fontSize)) {
-                wxFont font = textCtrl->GetFont();
-                font.SetPointSize(fontSize);
-                textCtrl->SetFont(font);
-            }
-        }
-    }
-
-    void OnFontStyle(wxCommandEvent& event) {
-        wxFontDialog fontDlg(this);
-        if (fontDlg.ShowModal() == wxID_OK) {
-            wxFont font = fontDlg.GetFontData().GetChosenFont();
-            textCtrl->SetFont(font);
-        }
-    }
 };
-
 
